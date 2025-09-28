ac40b5853708d0e22c46a7388c8c723dd86d663f
Jacek Langer <jacek.langer@outlook.de>
2025-09-28 09:48:47 +0200

    feat: implement pooling classes and interface for dimensionality reduction
    replaced the compression different pooling implementations in
    order to create a configurable pooling options.
    The user is now able to choose between different pooling models.

---
+++ NeuralNetwork.Core/Model/Data/Compression/Linear2DPooling.cs
@@ -0,0 +1,28 @@
+﻿using MathLib.Linalg;
+
+namespace NeuralNetworkLib.Model.Data.Compression;
+
+internal class Linear2DPooling(int width) : IPooling {
+
+    /// <summary>
+    ///     Compresses the input features from 784 to 196 using a simple 2x2 average pooling method.
+    /// </summary>
+    /// <param name="input">the input features</param>
+    /// <returns>compressed features as an array of size 196</returns>
+    /// <exception cref="Exception"></exception>
+    public Vector Pool(Vector input)
+    {
+        var height = input.Length / width;
+        Vector v = new(height / 2 * (width / 2));
+        var idx = 0;
+        for (var i = 0; i < height; i += 2)
+        for (var j = 0; j < width; j += 2)
+        {
+            var sum = input[i * width + j] + input[i * width + j + 1] + input[(i + 1) * width + j] +
+                input[(i + 1) * width + j + 1];
+            v[idx++] = sum / 4.0;
+        }
+
+        return v;
+    }
+}
